f134b8183e5da1279c67160add23759ec98be140
OlegWock <olegwock@gmail.com>
2018-09-04 19:19:12 +0300

    fix for nday

--- index.js
+++ index.js
@@ -83,13 +83,13 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 const REPLACES_URL = 'http://ccte.nau.edu.ua/images/Zameni.jpg';
 const REPLACES_LOCAL_PATH = Path.resolve(__dirname, 'replaces.jpg')
 const IDK = 'Sorry, but it\'s just placeholder';
-const SCHEDULE = [`Вихідний`,
-                  `1. Фіз. вих.\n2. Основи маркетингу\n3. Соціологія\n4. Проектний практикум`, 
-                  `1. Інструментальні засоби ВП\n2. Розробка веб-застосувань\n3. Основи маркетингу / Дискретна матем.`, 
-                  `1. Охорона праці\n2. Дискретна матем.\n3. Проектний практикум`,
-                  `1. Інструментальні засоби ВП\n2. Фіз. вих.\n3. Розробка веб-застосувань`,
-                  `1. none \n2. Конструювання ПЗ\n3. Розробка веб-застосувань\n4. Проектний практикум`,
-                  `Вихідний`];
+const SCHEDULE = [`Вихідний`, // Вс
+                  `1. Фіз. вих.\n2. Основи маркетингу\n3. Соціологія\n4. Проектний практикум`, // 1 - Пн
+                  `1. Інструментальні засоби ВП\n2. Розробка веб-застосувань\n3. Основи маркетингу / Дискретна матем.`, // 2 - Вт 
+                  `1. Охорона праці\n2. Дискретна матем.\n3. Проектний практикум`, // 3 - Ср
+                  `1. Інструментальні засоби ВП\n2. Фіз. вих.\n3. Розробка веб-застосувань`, // 4 - Чт
+                  `1. none \n2. Конструювання ПЗ\n3. Розробка веб-застосувань\n4. Проектний практикум`, // 5 - Пт
+                  `Вихідний`]; // 6 - Сб
 const START_TIME = Date.now() / 1000;
 const ADMINS_ID = [137307080]; // Feel free to write to me :)
 let students = JSON.parse(Fs.readFileSync('students.json', 'utf8'));
@@ -117,6 +117,7 @@ const staticAnswers = [{
         answer: () => {
             let cur_day = moment().day();
             if (cur_day > 4 || cur_day === 0) cur_day = 1;
+            else cur_day++;
             return SCHEDULE[cur_day];
         }
     },
